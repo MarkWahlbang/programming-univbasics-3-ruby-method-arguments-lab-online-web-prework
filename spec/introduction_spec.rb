b2611ae3b0d38dbbe6b477d4f70e30bf5657cc0b
brady sisk <bradysiskpro@yahoo.com>
2019-07-17 21:10:48 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
-describe "#introduction" do
+describe 
+end"#introduction" do
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
     expect{introduction("Dan")}.to output("Hi, my name is Dan.\n").to_stdout
   end
-end
 
 describe "#introduction_with_language" do
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do
